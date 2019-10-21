b556b3c3af70c0f565bbd0ab73ecd60535452069
luisguillermo <guillermo.velasco98@gmail.com>
2019-10-21 12:08:14 +0200

    Added read, and linked list functionalities.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.io.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        LinkedList<String> dictionary = new LinkedList<String>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("unsorteddict.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                dictionary.add(line);
+            }
+        }
+
+        System.out.println(dictionary);
+
+        Collections.sort(dictionary);
+        System.out.println(dictionary.toString());
+
+        FileWriter fw = new FileWriter("sorteddict.txt");
+
+        for (String temp : dictionary) {
+            fw.append(temp.toString());
+        }
+
     }
 }
