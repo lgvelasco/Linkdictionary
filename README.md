c0a1c6a49633344d429f3ca5cc0640e4acb4b544
luisguillermo <guillermo.velasco98@gmail.com>
2019-11-16 16:20:24 +0100

    Changes README.md

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 ##### By: Luis Guillermo Velasco & Carmen Oballe
 
 ## Refactor Branch
+Faster branch code was refactored using the following principles:
+* Meaningful names
+* Keep functions small
+* Avoid redundant commenting
+* Single responsibility principle
+* Don’t repeat yourself
+* Keep your code simple
+* You ain’t gonna need it
 
+###Examples
+Although changes where made through the entirety of the code, here
+are some examples:
+
+The method _compareTwoFiles()_ was renamed to _compareIfTwoFilesAreTheSame()_
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,47 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int compareTwoFiles(String file1, String file2) throws IOException {
+    public static void main(String[] args) throws IOException {
+
+        // performanceTestForAddingFromAFileAndWritingToAnother();
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("sortedtest.txt"));
+
+        Node start = new Node("discontents");
+
+        long first = System.currentTimeMillis();
+        start.insertFromFile("test.txt");
+        long last = System.currentTimeMillis();
+        System.out.println("Time to insert to the binary tree: " + (last-first) + " milliseconds");
+
+        first = System.currentTimeMillis();
+        start.addToFile(bw);
+        bw.close();
+        last = System.currentTimeMillis();
+        System.out.println("Time to add to a file: " + (last-first) + " milliseconds");
+
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 10; i++ ) {
+            System.out.println("Enter a command");
+            String command = scan.nextLine();
+
+            if(command.matches("^-?\\d+$")) {
+                int temp = Integer.parseInt(command);
+
+                if (temp == -1) {
+                    System.out.println(compareIfTwoTextFilesAreTheSame("sortedtest.txt", "sortedtest_2.txt"));
+                } else {
+                    System.out.println(wordAtIndex(temp, "sortedtest.txt"));
+                }
+            } else {
+                System.out.println(findWord(command, "sortedtest.txt"));
+            }
+
+        }
+
+    }
+
+    public static int compareIfTwoTextFilesAreTheSame(String file1, String file2) throws IOException {
 
         BufferedReader fileOne = new BufferedReader(new FileReader(file1));
         BufferedReader fileTwo = new BufferedReader(new FileReader(file2));
@@ -23,7 +63,7 @@ public class Main {
         return 0;
     }
 
-    public static void performanceTest() throws IOException {
+    public static void performanceTestForAddingFromAFileAndWritingToAnother() throws IOException {
 
         long insert = 0;
         long insertMin = 0;
@@ -75,46 +115,6 @@ public class Main {
         System.out.println("The average write time is: " + (write/10));
         System.out.println("Max time: " + writeMax);
         System.out.println("Min time: " + writeMin);
-
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        // performanceTest();
-
-        BufferedWriter bw = new BufferedWriter(new FileWriter("sortedtest.txt"));
-
-        Node start = new Node("discontents");
-
-        long first = System.currentTimeMillis();
-        start.insertFromFile("test.txt");
-        long last = System.currentTimeMillis();
-        System.out.println("Time to insert to the binary tree: " + (last-first) + " milliseconds");
-
-        first = System.currentTimeMillis();
-        start.addToFile(bw);
-        bw.close();
-        last = System.currentTimeMillis();
-        System.out.println("Time to add to a file: " + (last-first) + " milliseconds");
-
-        Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < 10; i++ ) {
-            System.out.println("Enter a command");
-            String command = scan.nextLine();
-
-            if(command.matches("^-?\\d+$")) {
-                int temp = Integer.parseInt(command);
-
-                if (temp == -1) {
-                    System.out.println(compareTwoFiles("sortedtest.txt", "sortedtest_2.txt"));
-                } else {
-                    System.out.println(wordAtIndex(temp, "sortedtest.txt"));
-                }
-            } else {
-                System.out.println(findWord(command, "sortedtest.txt"));
-            }
-
-        }
 
     }
 
